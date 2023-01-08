453565993f35fe3c037f2dfccc72beaf9485fe4e
gyanendra29-bca <gyanendratiwari200@gmail.com>
2023-01-08 21:59:15 +0530

    2nd commit

---
+++ ques2.java
@@ -0,0 +1,11 @@
+//WAP `Program to take float input and print it.
+public class ques2 
+{
+    public static void main(String[] args) 
+    {
+        float a=6.5f;
+        System.out.println("value of a is :"+a);
+    
+    }
+}
+
