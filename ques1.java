cba24f95047ab71b6dfba35459fc98e1c7e32e64
gyanendra29-bca <gyanendratiwari200@gmail.com>
2023-01-08 21:57:30 +0530

    1st commit

---
+++ ques1.java
@@ -0,0 +1,9 @@
+//WAP Program which store value 65 in computer memories and print it.
+public class ques1 
+{
+    public static void main(String[] args) {
+        int a=65;
+        System.out.println("value of a is :"+a);
+    }
+    
+}
