a24e127df46fd9e1ec14567abca56f1b2eb2540a
Gyanendra Tiwari <117968173+gyanendra29-bca@users.noreply.github.com>
2023-11-26 08:55:38 +0530

    Update aques13.java

--- aques13.java
+++ aques13.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class aques13 
 {
     public static void main(String[] args) 
-    {
+    { 
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter the array length:");
       int n=sc.nextInt();
